d7fb1478c56134bb9612f2df28a4e2bcc83f70e2
Vaishnavi Kumbhar <2023.vaishnavi.kumbhar@ves.ac.in>
2025-10-28 23:51:24 +0530

    Rersolved Proxy Error

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const path = require("path")
 
 const IndexRoute = require("./Routers/index")
+const story = require("./Routers/story")     // ✅ added this line
 const connectDatabase = require("./Helpers/database/connectDatabase")
 const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
 
@@ -13,27 +14,25 @@ dotenv.config({
 
 connectDatabase()
 
-const app = express() ;
+const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-app.use("/",IndexRoute)
+app.use("/", IndexRoute)
+app.use("/story", story)                      // ✅ added this line
 
 app.use(customErrorHandler)
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname , "public") ))
+app.use(express.static(path.join(__dirname , "public")))
 
-const server = app.listen(PORT,()=>{
-
-    console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
-
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} : ${process.env.NODE_ENV}`)
 })
 
-process.on("unhandledRejection",(err , promise) =>{
+process.on("unhandledRejection", (err , promise) => {
     console.log(`Logged Error : ${err}`)
-
-    server.close(()=>process.exit(1))
+    server.close(() => process.exit(1))
 })
